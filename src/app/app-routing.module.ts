d2866d0dec6925b44334526761ed7f3c38809df0
Alan Guerra <161255913+alanengenheirosoftware@users.noreply.github.com>
2024-02-28 18:16:25 -0300

    Página para criar serviço

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateOperationComponent } from './pages/create-operation/create-operation.component';
+import { CreateServiceComponent } from './pages/create-service/create-service.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: CreateServiceComponent
+  },
+  {
+    path: 'operacoes/criar',
     component: CreateOperationComponent
+  },
+  {
+    path: 'servicos/criar',
+    component: CreateServiceComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CreateOperationComponent } from './pages/create-operation/create-operation.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CreateServiceComponent } from './pages/create-service/create-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CreateOperationComponent,
-    SidebarComponent
+    SidebarComponent,
+    CreateServiceComponent
   ],
   imports: [
     BrowserModule,
